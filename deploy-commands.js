0eac8b2bddf1167611b1bd91473ee7f76b6064b6
kaxro01 <ka7xzz@gmail.com>
2026-09-12 07:36:53 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,48 +7,28 @@ const fs = require("fs");
 const path = require("path");
 
 const commands = [];
-
 const commandsPath = path.join(__dirname, "commands");
 
-if (fs.existsSync(commandsPath)) {
-    const commandFiles = fs
-        .readdirSync(commandsPath)
-        .filter(file => file.endsWith(".js"));
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter(file => file.endsWith(".js"));
 
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
 
-        if ("data" in command && "execute" in command) {
-            commands.push(command.data.toJSON());
-        }
+    if ("data" in command && "execute" in command) {
+        commands.push(command.data.toJSON());
+        console.log(`Found command: ${command.data.name}`);
     }
 }
 
-if (!process.env.BOT_TOKEN) {
-    console.error("BOT_TOKEN is missing!");
-    process.exit(1);
-}
+const rest = new REST({ version: "10" })
+    .setToken(process.env.BOT_TOKEN);
 
-if (!process.env.CLIENT_ID) {
-    console.error("CLIENT_ID is missing!");
-    process.exit(1);
-}
-
-if (!process.env.GUILD_ID) {
-    console.error("GUILD_ID is missing!");
-    process.exit(1);
-}
-
-const rest = new REST({
-    version: "10"
-}).setToken(process.env.BOT_TOKEN);
-
-async function deploy() {
+(async () => {
     try {
-        console.log(
-            `Deploying ${commands.length} command(s)...`
-        );
+        console.log(`Deploying ${commands.length} command(s)...`);
 
         await rest.put(
             Routes.applicationGuildCommands(
@@ -60,12 +40,8 @@ async function deploy() {
             }
         );
 
-        console.log(
-            "All slash commands deployed successfully."
-        );
+        console.log("All slash commands deployed successfully.");
     } catch (error) {
         console.error(error);
     }
-}
-
-deploy();
+})();
